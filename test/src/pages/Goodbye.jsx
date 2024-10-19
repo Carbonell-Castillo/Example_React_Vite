19933b08a8f712f15e90da40111c668bceff8e9a
Carbonell-Castillo <brucecastillo1204@gmail.com>
2024-10-18 19:31:29 -0600

    Backend and fronted finished

--- test/src/pages/Goodbye.jsx
+++ test/src/pages/Goodbye.jsx
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Goodbye = () => {
-    return (
-      <div>
-        <h1>¡Adiós!</h1>
-        <p>Esta es la página de despedida.</p>
-      </div>
-    );
-  };
-  
-  export default Goodbye;
-  
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/goodbye')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching goodbye message:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>Esta es la página de despedida.</p>
+    </div>
+  );
+};
+
+export default Goodbye;
--- test/src/pages/Welcome.jsx
+++ test/src/pages/Welcome.jsx
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Welcome = () => {
-    return (
-      <div>
-        <h1>¡Bienvenido!</h1>
-        <p>Esta es la página de bienvenida.</p>
-      </div>
-    );
-  };
-  
-  export default Welcome;
-  
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/welcome')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching welcome message:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>Esta es la página de bienvenida.</p>
+    </div>
+  );
+};
+
+export default Welcome;
